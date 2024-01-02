cd46bc7d3efafdcc3ca611b0f93a2e5c19a45a40
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2024-01-02 21:56:44 +0200

    Swap bss and data sections

--- Core/Startup/startup.c
+++ Core/Startup/startup.c
@@ -13,13 +13,13 @@ static void init_data(uint32_t *start, uint32_t *end, const uint32_t *loadStart)
 }
 
 #define INIT_SECTION(name) do { \
-	extern uint32_t _##name##_bss_run_addr; \
-	extern uint32_t _##name##_bss_end; \
 	extern uint32_t _##name##_data_run_addr; \
 	extern uint32_t _##name##_data_end; \
 	extern const uint32_t _##name##_data_load_addr; \
-	init_bss(&_##name##_bss_run_addr, &_##name##_bss_end); \
+	extern uint32_t _##name##_bss_run_addr; \
+	extern uint32_t _##name##_bss_end; \
 	init_data(&_##name##_data_run_addr, &_##name##_data_end, &_##name##_data_load_addr); \
+	init_bss(&_##name##_bss_run_addr, &_##name##_bss_end); \
 	} while (0)
 
 
