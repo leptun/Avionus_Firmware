10e10005680d0310b79c43ad50f72913ee5250be
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2024-01-06 11:49:48 +0100

    Add freertos error callbacks

--- Kernel/FreeRTOSConfig.h
+++ Kernel/FreeRTOSConfig.h
@@ -90,10 +90,10 @@
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                      0
 #define configUSE_TICK_HOOK                      0
-#define configCHECK_FOR_STACK_OVERFLOW           0
-#define configUSE_MALLOC_FAILED_HOOK             0
+#define configCHECK_FOR_STACK_OVERFLOW           2
+#define configUSE_MALLOC_FAILED_HOOK             1
 #define configUSE_DAEMON_TASK_STARTUP_HOOK       1
-#define configUSE_SB_COMPLETED_CALLBACK          0
+#define configUSE_SB_COMPLETED_CALLBACK          1
 
 /* Run time and task stats gathering related definitions. */
 #define configGENERATE_RUN_TIME_STATS            0
