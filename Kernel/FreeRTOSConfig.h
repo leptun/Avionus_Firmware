5728f1983c087fef6850ae32329f03871a87421c
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2024-01-25 10:36:50 +0100

    A bit more fiddling with the stacks

--- Kernel/FreeRTOSConfig.h
+++ Kernel/FreeRTOSConfig.h
@@ -58,7 +58,7 @@
 #define configSYSTICK_CLOCK_HZ                   ( configCPU_CLOCK_HZ / 8 )
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 32 )
-#define configMINIMAL_STACK_SIZE                 ((uint16_t)64)
+#define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configUSE_16_BIT_TICKS                   0
 #define configIDLE_SHOULD_YIELD                  1
--- Kernel/static_buffers.c
+++ Kernel/static_buffers.c
@@ -1,7 +1,7 @@
 #include "FreeRTOS.h"
 
 static StaticTask_t xIdleTaskTCBBuffer;
-static StackType_t xIdleStack[configMINIMAL_STACK_SIZE]  __attribute__((aligned(configMINIMAL_STACK_SIZE*4)));
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE]  __attribute__((aligned(configMINIMAL_STACK_SIZE*4))) __attribute__((section(".stack")));
 void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
 {
   *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
@@ -10,7 +10,7 @@ void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackTy
 }
 
 static StaticTask_t xTimerTaskTCBBuffer;
-static StackType_t xTimerStack[configTIMER_TASK_STACK_DEPTH]  __attribute__((aligned(configTIMER_TASK_STACK_DEPTH*4)));
+static StackType_t xTimerStack[configTIMER_TASK_STACK_DEPTH]  __attribute__((aligned(configTIMER_TASK_STACK_DEPTH*4))) __attribute__((section(".stack")));
 void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize )
 {
   *ppxTimerTaskTCBBuffer = &xTimerTaskTCBBuffer;
