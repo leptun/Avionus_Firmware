9feb5695f32d327971b32b2539694b48e350099b
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2024-02-18 15:20:18 +0100

    Openocd support

--- glue/startup.c
+++ glue/startup.c
@@ -1,4 +1,5 @@
 #include <inttypes.h>
+#include <main.h>
 
 static void init_bss(uint32_t *start, uint32_t *end) {
 	uint32_t cnt = end - start;
@@ -24,6 +25,24 @@ static void init_data(uint32_t *start, uint32_t *end, const uint32_t *loadStart)
 
 
 void init_sections(void) {
+	extern uint32_t g_pfnVectors[];
+	SCB->VTOR = (uint32_t)g_pfnVectors;
+
+	HAL_DeInit();
+    // reset clocks
+    SET_BIT(RCC->CR, RCC_CR_HSION); /* Set HSION bit to the reset value */
+    while (READ_BIT(RCC->CR, RCC_CR_HSIRDY) == RESET) { } /* Wait till HSI is ready */
+    CLEAR_REG(RCC->CFGR); /* Reset CFGR register */
+    while (READ_BIT(RCC->CFGR, RCC_CFGR_SWS) != RESET) { } /* Wait till clock switch is ready */
+    CLEAR_BIT(RCC->CR, RCC_CR_PLLON); /* Clear PLLON bit */
+    while (READ_BIT(RCC->CR, RCC_CR_PLLRDY) != RESET) { } /* Wait till PLL is disabled */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    PWR->CR1 = 0x0000C000;
+    PWR->CSR1 = 0x00000000;
+    PWR->CR2 = 0x00000000;
+    PWR->CSR2 = 0x00000000;
+    __HAL_RCC_PWR_CLK_DISABLE();
+
 	INIT_SECTION(freertos);
 	INIT_SECTION(app);
 	INIT_SECTION(tinyusb);
